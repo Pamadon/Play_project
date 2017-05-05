3dd38be798fb92c6251ae8220eaa98b4089f4375
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-05-05 13:55:03 -0700

    testing user id

--- controllers/find.js
+++ controllers/find.js
@@ -6,7 +6,7 @@ var isLoggedIn = require('../middleware/isLoggedIn');
 
 
 router.get('/', isLoggedIn, function(req, res) {
-    ActiveGames.find({}, function(error, places) {
+    User.find({}, function(error, places) {
 
         res.render('find', { places: places });
     });
